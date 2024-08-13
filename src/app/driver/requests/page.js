fb18fdee4afee610bfa6450ca660bbfe30f610d7
Omansh Sharma <74657563+arduR-O@users.noreply.github.com>
2024-08-13 19:04:47 +0530

    update: minor update to driver view

--- src/app/driver/requests/page.js
+++ src/app/driver/requests/page.js
@@ -64,49 +64,49 @@ const RequestsContent = () => {
           No pending requests
         </p>
       )}
+      <div className="h-[50px] w-full max-w-[40vw] mt-4">
+        {successMessage ? (
+          <div
+            className={`flex items-center p-4 text-sm text-green-500 rounded-lg bg-green-50 dark:bg-black dark:text-grey border-2 border-green-500`}
+            role="alert"
+          >
+            <svg
+              className="flex-shrink-0 inline w-4 h-4 me-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="green"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+            </svg>
+            <span className="sr-only">Success</span>
+            <div>
+              <span className="font-medium">Success!</span> {successMessage}
+            </div>
+          </div>
+        ) : errorMessage ? (
+          <div
+            className={`flex items-center p-4 text-sm text-orange rounded-lg bg-yellow-50 dark:bg-black dark:text-grey border-2 border-orange`}
+            role="alert"
+          >
+            <svg
+              className="flex-shrink-0 inline w-4 h-4 me-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="orange"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+            </svg>
+            <span className="sr-only">Info</span>
+            <div>
+              <span className="font-medium">Alert!</span> {errorMessage}
+            </div>
+          </div>
+        ) : null}
+      </div>
       {requests.length > 0 && (
         <div className="overflow-x-auto">
-          <div className="h-[50px] w-full max-w-[40vw] mt-4">
-            {successMessage ? (
-              <div
-                className={`flex items-center p-4 text-sm text-green-500 rounded-lg bg-green-50 dark:bg-black dark:text-grey border-2 border-green-500`}
-                role="alert"
-              >
-                <svg
-                  className="flex-shrink-0 inline w-4 h-4 me-3"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="green"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-                </svg>
-                <span className="sr-only">Success</span>
-                <div>
-                  <span className="font-medium">Success!</span> {successMessage}
-                </div>
-              </div>
-            ) : errorMessage ? (
-              <div
-                className={`flex items-center p-4 text-sm text-orange rounded-lg bg-yellow-50 dark:bg-black dark:text-grey border-2 border-orange`}
-                role="alert"
-              >
-                <svg
-                  className="flex-shrink-0 inline w-4 h-4 me-3"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="orange"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-                </svg>
-                <span className="sr-only">Info</span>
-                <div>
-                  <span className="font-medium">Alert!</span> {errorMessage}
-                </div>
-              </div>
-            ) : null}
-          </div>
           <table className="w-full text-left border-collapse">
             <thead>
               <tr className="bg-gray-200">
