4b7a6c080a0c2fcc25508489e58d5bf98e8b2cc7
Omansh Sharma <74657563+arduR-O@users.noreply.github.com>
2025-02-26 17:33:36 +0530

    fix: driver home cls

--- src/app/driver/home/page.js
+++ src/app/driver/home/page.js
@@ -2,24 +2,33 @@
 import Link from "next/link";
 import OrgButton from "@/components/OrgButton";
 import Header from "@/components/Header";
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 const HomeContent = () => {
   const searchParams = useSearchParams();
   const driverId = searchParams.get("driverId");
-  
+
   return (
-          <main className="min-h-screen min-w-screen flex flex-col justify-center items-center gap-2">
-              <Header content="Driver Home" showBackButton={false}/>
-              <div className="flex flex-col h-[55vh] w-[25vw] justify-evenly border-4 border-grey rounded-3xl items-center">
-                  <Link href={`/driver/requests?driverId=${encodeURIComponent(driverId)}`}>
-                      <OrgButton content="View Requests" />
-                  </Link>
-                  <Link href={`/driver/schedule?driverId=${encodeURIComponent(driverId)}`}>
-                      <OrgButton content="View Schedule" />
-                  </Link>
-              </div>
-          </main>
+    <main className="min-h-screen min-w-screen flex flex-col justify-center items-center gap-2">
+      <Header content="Driver Home" showBackButton={false} />
+      <div className="flex flex-col h-[55vh] w-[25vw] justify-evenly border-4 border-grey rounded-3xl items-center">
+        <div className="w-48 h-20 flex justify-center items-center">
+          <Link
+            href={`/driver/requests?driverId=${encodeURIComponent(driverId)}`}
+          >
+            <OrgButton content="View Requests" />
+          </Link>
+        </div>
+
+        <div className="w-48 h-20 flex justify-center items-center">
+          <Link
+            href={`/driver/schedule?driverId=${encodeURIComponent(driverId)}`}
+          >
+            <OrgButton content="View Schedule" />
+          </Link>
+        </div>
+      </div>
+    </main>
   );
 };
 const Home = () => (
--- src/app/manager/drivers/page.jsx
+++ src/app/manager/drivers/page.jsx
@@ -1,90 +1,122 @@
-"use client"
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
-import Header from '@/components/Header';
+"use client";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import Header from "@/components/Header";
 const Drivers = () => {
-    const [drivers, setDrivers] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [filteredDrivers, setFilteredDrivers] = useState([]);
+  const [drivers, setDrivers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredDrivers, setFilteredDrivers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        async function fetchDrivers() {
-            try {
-                const response = await fetch('/api/get-drivers');
-                const data = await response.json();
-                setDrivers(data);
-                setFilteredDrivers(data);
-            } catch (error) {
-                console.error('Error fetching drivers:', error);
-            }
-        }
-        fetchDrivers();
-    }, []);
+  useEffect(() => {
+    async function fetchDrivers() {
+      try {
+        setIsLoading(true);
+        const response = await fetch("/api/get-drivers");
+        const data = await response.json();
+        setDrivers(data);
+        setFilteredDrivers(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching drivers:", error);
+        setIsLoading(false);
+      }
+    }
+    fetchDrivers();
+  }, []);
 
-    const handleSearch = (event) => {
-        const term = event.target.value.toLowerCase();
-        setSearchTerm(term);
-        const filtered = drivers.filter(driver =>
-            driver.name.toLowerCase().includes(term) ||
-            driver.phone.toString().includes(term)
-        );
-        setFilteredDrivers(filtered);
-    };
-
-    return (
-        <div className="p-4 flex flex-col gap-4 items-center">
-            <Header content="View Drivers"/>
-            <input
-                type="text"
-                placeholder="Search by name or number"
-                value={searchTerm}
-                onChange={handleSearch}
-                className='text-black mb-4 p-2 border rounded self-start'
-            />
-            <div className="relative overflow-x-auto shadow-md sm:rounded-lg self-start w-full">
-                <table className="w-full text-left border-collapse">
-                    <thead>
-                        <tr className="bg-gray-200">
-                            <th scope="col" className="p-3 font-bold uppercase text-gray-600 border-b">
-                                Driver ID
-                            </th>
-                            <th scope="col" className="p-3 font-bold uppercase text-gray-600 border-b">
-                                Driver Name
-                            </th>
-                            <th scope="col" className="p-3 font-bold uppercase text-gray-600 border-b">
-                                Phone
-                            </th>
-                            <th scope="col" className="p-3 font-bold uppercase text-gray-600 border-b">
-                                <span className="sr-only">Details</span>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {filteredDrivers.map(driver => (
-                            <tr key={driver.id} className="bg-black hover:bg-orange">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    {driver.id}
-                                </th>
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    {driver.name}
-                                </th>
-                                <td className="px-6 py-4">
-                                    {driver.phone}
-                                </td>
-                                <td className="px-6 py-4 text-right">
-                                    <Link href={`/manager/drivers/details/${encodeURIComponent(driver.id)}`}>
-                                        <span className="font-medium text-blue-600 dark:text-blue-500 hover:text-black cursor-pointer">
-                                            Details
-                                        </span>
-                                    </Link>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </div>
+  const handleSearch = (event) => {
+    const term = event.target.value.toLowerCase();
+    setSearchTerm(term);
+    const filtered = drivers.filter(
+      (driver) =>
+        driver.name.toLowerCase().includes(term) ||
+        driver.phone.toString().includes(term)
     );
+    setFilteredDrivers(filtered);
+  };
+
+  return (
+    <div className="p-4 flex flex-col gap-4 items-center">
+      <Header content="View Drivers" />
+      <input
+        type="text"
+        placeholder="Search by name or number"
+        value={searchTerm}
+        onChange={handleSearch}
+        className="text-black mb-4 p-2 border rounded self-start"
+      />
+      {isLoading ? (
+        <div className="flex items-center justify-center">
+          <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-white mr-2"></div>
+          Loading...
+        </div>
+      ) : (
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg self-start w-full">
+          <table className="w-full text-left border-collapse">
+            <thead>
+              <tr className="bg-gray-200">
+                <th
+                  scope="col"
+                  className="p-3 font-bold uppercase text-gray-600 border-b"
+                >
+                  Driver ID
+                </th>
+                <th
+                  scope="col"
+                  className="p-3 font-bold uppercase text-gray-600 border-b"
+                >
+                  Driver Name
+                </th>
+                <th
+                  scope="col"
+                  className="p-3 font-bold uppercase text-gray-600 border-b"
+                >
+                  Phone
+                </th>
+                <th
+                  scope="col"
+                  className="p-3 font-bold uppercase text-gray-600 border-b"
+                >
+                  <span className="sr-only">Details</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredDrivers.map((driver) => (
+                <tr key={driver.id} className="bg-black hover:bg-orange">
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {driver.id}
+                  </th>
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {driver.name}
+                  </th>
+                  <td className="px-6 py-4">{driver.phone}</td>
+                  <td className="px-6 py-4 text-right">
+                    <Link
+                      href={`/manager/drivers/details/${encodeURIComponent(
+                        driver.id
+                      )}`}
+                    >
+                      <span className="font-medium text-blue-600 dark:text-blue-500 hover:text-black cursor-pointer">
+                        Details
+                      </span>
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Drivers;
